aa79344cd49d5c3c684f9e73da660408f56bb030
Sérgio Pereira da Silva <sergio.pds@outlook.pt>
2022-04-11 23:50:45 +0100

    Update server.js
    Added route parsing for '/api' route

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const app = express();
+
+const api = require('./api')
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +13,8 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+// Routing to API handler
+app.use('/api', api);
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
